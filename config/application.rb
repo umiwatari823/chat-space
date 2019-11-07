282479afe76e422611d14da6c781b0cd37c48c0e
umiwatari823 <umiwatari823@gmail.com>
2019-11-07 21:17:53 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
-      config.time_zone = ‘Tokyo’
     end
+      config.i18n.default_locale = :ja
+      config.time_zone = 'Tokyo'
   end
 end
